f14a411ddece03ab107770bc9fe1452442579cea
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-08-21 16:46:33 -0700

    Install graph-tools on Linux on CI

--- ci/install_foreign_graph_packages.py
+++ ci/install_foreign_graph_packages.py
@@ -2,13 +2,33 @@
 import sys
 import subprocess as sp
 
-# Networkx via pip
-call = 'pip install networkx'
-call = call.split()
-try:
-    sp.run(call)
-except AttributeError:
-    sp.call(call)
+
+# Networkx is installed in tox later on
 
 
-# Graph-tool (TODO)
+# Graph-tool
+calltext = ''
+if sys.platform == 'linux':
+    calltext += '''
+    sudo deb http://downloads.skewed.de/apt/bionic bionic universe
+    sudo deb-src http://downloads.skewed.de/apt/bionic bionic universe
+    sudo apt-key adv --keyserver keys.openpgp.org --recv-key 612DEFB798507F25
+    '''
+    calltext += '\n'
+    if sys.version[0] == '2':
+        calltext += 'sudo apt-get install python-graph-tool'
+    else:
+        calltext += 'sudo apt-get install python3-graph-tool'
+
+else:
+    print('Graph-tool installation on OSX and Windows missing for now')
+
+for call in calltext.split('\n'):
+    if call == '':
+        continue
+    print(call)
+    call = call.split()
+    try:
+        sp.run(call)
+    except AttributeError:
+        sp.call(call)
