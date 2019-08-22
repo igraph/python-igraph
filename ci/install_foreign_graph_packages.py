72a7517df026b040f18cd1ac397e7aa5c8568d2f
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-08-21 17:06:39 -0700

    Bugfix in graph-tool installation, n2

--- ci/install_foreign_graph_packages.py
+++ ci/install_foreign_graph_packages.py
@@ -13,13 +13,13 @@ except AttributeError:
 
 # Graph-tool
 if sys.platform == 'linux':
-    runfun('sudo echo "deb http://downloads.skewed.de/apt/bionic bionic universe" >> /etc/apt/sources.list"')
-    runfun('sudo echo "deb-src http://downloads.skewed.de/apt/bionic bionic universe" >> /etc/apt/sources.list"')
-    runfun('sudo apt-key adv --keyserver keys.openpgp.org --recv-key 612DEFB798507F25')
+    runfun('sudo echo "deb http://downloads.skewed.de/apt/bionic bionic universe" >> /etc/apt/sources.list"', shell=True)
+    runfun('sudo echo "deb-src http://downloads.skewed.de/apt/bionic bionic universe" >> /etc/apt/sources.list"', shell=True)
+    runfun('sudo apt-key adv --keyserver keys.openpgp.org --recv-key 612DEFB798507F25', shell=True)
     if sys.version[0] == '2':
-        runfun('sudo apt-get install python-graph-tool')
+        runfun('sudo apt-get install python-graph-tool', shell=True)
     else:
-        runfun('sudo apt-get install python3-graph-tool')
+        runfun('sudo apt-get install python3-graph-tool', shell=True)
 
 else:
     print('Graph-tool installation on OSX and Windows missing for now')
