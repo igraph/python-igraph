db22084f40636e881e34365cb93a4744a109eb46
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-08-21 17:29:49 -0700

    Try again to install graph-tool on ubuntu

--- ci/install_foreign_graph_packages.py
+++ ci/install_foreign_graph_packages.py
@@ -15,7 +15,9 @@ except AttributeError:
 if sys.platform == 'linux':
     runfun('sudo echo "deb http://downloads.skewed.de/apt/bionic bionic universe" >> /etc/apt/sources.list"', shell=True)
     runfun('sudo echo "deb-src http://downloads.skewed.de/apt/bionic bionic universe" >> /etc/apt/sources.list"', shell=True)
-    runfun('sudo apt-key adv --keyserver keys.openpgp.org --recv-key 612DEFB798507F25', shell=True)
+    runfun('gpg --keyserver keys.openpgp.org --recv 612DEFB798507F25', shell=True)
+    runfun('gpg --export --armor 612DEFB798507F25 | sudo apt-key add -', shell=True)
+    #runfun('sudo apt-key adv --keyserver keys.openpgp.org --recv-key 612DEFB798507F25', shell=True)
     if sys.version[0] == '2':
         runfun('sudo apt-get install python-graph-tool', shell=True)
     else:
