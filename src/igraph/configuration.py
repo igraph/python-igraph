1ef2215c4c40183eb810b28ba08cdc5690655d67
Thierry Thomas <thierry@FreeBSD.org>
2021-01-02 10:30:26 +0000

    Adding an image viewer for FreeBSD (#354)

--- src/igraph/configuration.py
+++ src/igraph/configuration.py
@@ -56,6 +56,18 @@ def get_platform_image_viewer():
                 if os.path.isfile(full_path):
                     return full_path
         return ""
+    elif plat == "FreeBSD":
+        # FreeBSD also has a whole lot of choices, try to find one
+        choices = ["eog", "gthumb", "geeqie", "display",
+                   "gpicview", "gwenview", "qiv", "gimv", "ristretto",
+                   "geeqie", "eom"]
+        paths = ["%%LOCALBASE%%/bin"]
+        for path in paths:
+            for choice in choices:
+                full_path = os.path.join(path, choice)
+                if os.path.isfile(full_path):
+                    return full_path
+        return ""
     elif plat == "Windows" or plat == "Microsoft":    # Thanks to Dale Hunscher
         # Use the built-in Windows image viewer, if available
         return "start"
