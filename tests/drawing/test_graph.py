0b165a1f7f1c7103a7257492d5d8c36a09dfc351
Tamas Nepusz <ntamas@gmail.com>
2021-08-02 15:34:14 +0200

    chore: reformatted tests/drawing with black

--- tests/drawing/test_graph.py
+++ tests/drawing/test_graph.py
@@ -3,6 +3,7 @@ import unittest
 
 
 from igraph import Graph, InternalError, plot, VertexClustering
+
 # FIXME: find a better way to do this that works for both direct call and module
 # import e.g. tox
 try:
@@ -22,75 +23,69 @@ image_comparison = find_image_comparison()
 
 class GraphTestRunner(unittest.TestCase):
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
-    @image_comparison(
-            baseline_images=['graph_basic'], remove_text=True)
+    @image_comparison(baseline_images=["graph_basic"], remove_text=True)
     def test_basic(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(5)
         fig, ax = plt.subplots()
         plot(g, target=ax)
 
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
-    @image_comparison(
-            baseline_images=['graph_directed'], remove_text=True)
+    @image_comparison(baseline_images=["graph_directed"], remove_text=True)
     def test_directed(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(5, directed=True)
         fig, ax = plt.subplots()
         plot(g, target=ax)
 
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
-    @image_comparison(
-            baseline_images=['graph_mark_groups_directed'], remove_text=True)
+    @image_comparison(baseline_images=["graph_mark_groups_directed"], remove_text=True)
     def test_mark_groups(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(5, directed=True)
         fig, ax = plt.subplots()
         plot(g, target=ax, mark_groups=True)
 
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
     @image_comparison(
-            baseline_images=['graph_mark_groups_squares_directed'], remove_text=True)
+        baseline_images=["graph_mark_groups_squares_directed"], remove_text=True
+    )
     def test_mark_groups_squares(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(5, directed=True)
         fig, ax = plt.subplots()
-        plot(g, target=ax, mark_groups=True, vertex_shape='s')
+        plot(g, target=ax, mark_groups=True, vertex_shape="s")
 
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
-    @image_comparison(
-            baseline_images=['graph_edit_children'], remove_text=True)
+    @image_comparison(baseline_images=["graph_edit_children"], remove_text=True)
     def test_mark_groups_squares(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(5)
         fig, ax = plt.subplots()
-        plot(g, target=ax, vertex_shape='o')
+        plot(g, target=ax, vertex_shape="o")
         dot = ax.get_children()[0]
-        dot.set_facecolor('blue')
+        dot.set_facecolor("blue")
         dot.radius *= 0.5
 
 
 class ClusteringTestRunner(unittest.TestCase):
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
-    @image_comparison(
-            baseline_images=['clustering_directed'], remove_text=True)
+    @image_comparison(baseline_images=["clustering_directed"], remove_text=True)
     def test_clustering_directed_small(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(5, directed=True)
         clu = VertexClustering(g, [0] * 5)
         fig, ax = plt.subplots()
         plot(clu, target=ax, mark_groups=True)
 
     @unittest.skipIf(plt is None, "test case depends on matplotlib")
-    @image_comparison(
-            baseline_images=['clustering_directed_large'], remove_text=True)
+    @image_comparison(baseline_images=["clustering_directed_large"], remove_text=True)
     def test_clustering_directed_large(self):
-        plt.close('all')
+        plt.close("all")
         g = Graph.Ring(50, directed=True)
         clu = VertexClustering(g, [0] * 3 + [1] * 17 + [2] * 30)
         fig, ax = plt.subplots()
         plot(clu, target=ax, mark_groups=True)
-
 
 
 def suite():
