b7aabb15dc728ed00a7c3c58d3a3fbb476f0dde0
Tamas Nepusz <ntamas@gmail.com>
2021-09-10 14:23:56 +0200

    feat: test.sh now allows cleaning the compiled igraph C core

--- test.sh
+++ test.sh
@@ -7,14 +7,31 @@ PYTHON=python3
 
 set -e
 
+CLEAN=0
+
+while getopts ":c" OPTION; do
+    case $OPTION in
+        c)
+            CLEAN=1
+            ;;
+        \?)
+            echo "Usage: $0 [-c]"
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
 if [ ! -d $VENV_DIR ]; then
-	$PYTHON -m venv $VENV_DIR
+    $PYTHON -m venv $VENV_DIR
     $VENV_DIR/bin/pip install -U pip wheel
 fi
 
 rm -rf build/
+if [ x$CLEAN = x1 ]; then
+    rm -rf vendor/build vendor/install
+fi
+
 $VENV_DIR/bin/python setup.py build
 $VENV_DIR/bin/pip install --use-feature=in-tree-build .[plotting,test]
 $VENV_DIR/bin/pytest tests
-
 
