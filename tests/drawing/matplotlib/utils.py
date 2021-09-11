7883f1b397d7f7b3461090385731c59ee0539fb4
Fabio Zanini <fabio.zanini@fastmail.fm>
2021-09-12 08:11:26 +1000

    Back to PDF (should work on i686) and install ghostscript on CI

--- tests/drawing/matplotlib/utils.py
+++ tests/drawing/matplotlib/utils.py
@@ -26,7 +26,7 @@ def find_image_comparison():
 
 
 # NOTE: Parametrizing this requires pytest (see matplotlib's test suite)
-_default_extension = "png"
+_default_extension = "pdf"
 
 
 def _unittest_image_comparison(
