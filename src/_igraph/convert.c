be01f0162caa67c14986de3ff68d40a607498c82
Tamas Nepusz <ntamas@gmail.com>
2021-10-14 15:00:11 +0200

    fix: fix crash in Graph.Realize_Degree_Sequence() on Python 3.7 (maybe other versions)

--- src/_igraph/convert.c
+++ src/_igraph/convert.c
@@ -3143,6 +3143,7 @@ int igraphmodule_PyObject_to_pagerank_algo_t(PyObject *o, igraph_pagerank_algo_t
  * \brief Converts a Python object to an igraph \c igraph_edge_type_sw_t
  */
 int igraphmodule_PyObject_to_edge_type_sw_t(PyObject *o, igraph_edge_type_sw_t *result) {
+  int result_int, retval;
   static igraphmodule_enum_translation_table_entry_t edge_type_sw_tt[] = {
         {"simple", IGRAPH_SIMPLE_SW},
         {"loops", IGRAPH_LOOPS_SW},
@@ -3151,7 +3152,14 @@ int igraphmodule_PyObject_to_edge_type_sw_t(PyObject *o, igraph_edge_type_sw_t *
         {0,0}
     };
 
-  return igraphmodule_PyObject_to_enum_strict(o, edge_type_sw_tt, (int*)result);
+  retval = igraphmodule_PyObject_to_enum_strict(o, edge_type_sw_tt, &result_int);
+
+  if (retval) {
+    return retval;
+  }
+
+  *result = result_int;
+  return 0;
 }
 
 /**
