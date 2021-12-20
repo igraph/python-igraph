a66f132ce2dee0bea749cae848af165031343df5
Gomango999 <kv.zhu999@gmail.com>
2021-12-21 01:02:41 +1100

    Make matching in bipartite_matching.rst more visible

--- doc/source/tutorials/bipartite_matching/assets/bipartite_matching.py
+++ doc/source/tutorials/bipartite_matching/assets/bipartite_matching.py
@@ -27,7 +27,8 @@ ig.plot(
     vertex_size=0.4,
     vertex_label=range(g.vcount()),
     vertex_color="lightblue",
-    edge_width=[2.5 if e.target == matching.match_of(e.source) else 1.0 for e in g.es]
+    edge_width=[3 if e.target == matching.match_of(e.source) else 1.0 for e in g.es],
+    edge_color=["red" if e.target == matching.match_of(e.source) else "black" for e in g.es]
 )
 plt.show()
 
