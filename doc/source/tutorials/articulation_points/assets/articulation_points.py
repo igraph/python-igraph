cb11c726a328cdcd23185d9acded7348c62ff54d
Gomango999 <kv.zhu999@gmail.com>
2021-12-21 00:44:17 +1100

    Update articulation_points code style

--- doc/source/tutorials/articulation_points/assets/articulation_points.py
+++ doc/source/tutorials/articulation_points/assets/articulation_points.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 g = ig.Graph(edges=[(0, 1), (1, 2), (2, 0), (2, 3), (3, 4), (4, 5),     
         (5, 6), (6, 3), (3, 5), (4, 6), (3, 7), (7, 8)])
 
-articulation_points = g.articulation_points()
+# Find the set of articulation points as a vertex sequence
+articulation_points = g.vs[g.articulation_points()]
 
 # Plot graph
 fig, ax = plt.subplots()
@@ -15,8 +16,8 @@ ig.plot(
     vertex_size=0.3,
     vertex_color="lightblue",
     vertex_label=range(g.vcount()),
-    vertex_frame_color = ["red" if i in articulation_points else "black" for i in range(g.vcount())],
-    vertex_frame_width = [3 if i in articulation_points else 1 for i in range(g.vcount())],
+    vertex_frame_color = ["red" if v in articulation_points else "black" for v in g.vs],
+    vertex_frame_width = [3 if v in articulation_points else 1 for v in g.vs],
     edge_width=0.8,
     edge_color='gray'
 )
