6e077ea14d67f78c034c76e679ae76ba108b76b0
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-01-12 07:43:36 +1100

    Coding and wording styles

--- doc/source/tutorials/betweenness/assets/betweenness.py
+++ doc/source/tutorials/betweenness/assets/betweenness.py
@@ -1,11 +1,20 @@
-import igraph as ig
+import random
 import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import LinearSegmentedColormap, Normalize
-import random
+import igraph as ig
 
-def plot_betweenness(g, fig, ax):
-    
+
+def plot_betweenness(g, ax, cax1, cax2):
+    '''Plot vertex/edge betweenness, with colorbars
+
+    Args:
+        g: the graph to plot.
+        ax: the Axes for the graph
+        cax1: the Axes for the vertex betweenness colorbar
+        cax2: the Axes for the edge betweenness colorbar
+    '''
+
     # Calculate vertex betweenness and scale it to be between 0.0 and 1.0
     vertex_betweenness = g.betweenness()
     edge_betweenness = g.edge_betweenness()
@@ -14,54 +23,43 @@ def plot_betweenness(g, fig, ax):
     print(f"vertices: {min(vertex_betweenness)} - {max(vertex_betweenness)}")
     print(f"edges: {min(edge_betweenness)} - {max(edge_betweenness)}")
 
-    # Define color bars
+    # Define mappings betweenness -> color
     cmap1 = LinearSegmentedColormap.from_list("vertex_cmap", ["pink", "indigo"])
     cmap2 = LinearSegmentedColormap.from_list("edge_cmap", ["lightblue", "midnightblue"])
-
-    # Create normalising objects to scale the color bars correctly
-    norm1 = Normalize()
-    norm1.autoscale(vertex_betweenness)
-    norm2 = Normalize()
-    norm2.autoscale(edge_betweenness)
-
-    # Create new axes for color bars
-    cax1 = fig.add_axes([ax.get_position().x0, ax.get_position().y0-0.05, ax.get_position().width, 0.03], in_layout=True)
-    cax2 = fig.add_axes([ax.get_position().x0, ax.get_position().y0-0.20, ax.get_position().width, 0.03], in_layout=True)
-
-    plt.colorbar(ScalarMappable(norm=norm1, cmap=cmap1), cax=cax1, orientation="horizontal")
-    plt.colorbar(ScalarMappable(norm=norm2, cmap=cmap2), cax=cax2, orientation="horizontal")
-
-    # Add labels for color bars
-    mid = (ax.get_position().x0 + ax.get_position().x1) / 2
-    fig.text(mid, 0.3, 'Vertex Betweenness', ha='center')
-    fig.text(mid, 0.15, 'Edge Betweenness', ha='center')
 
     # Plot graph
     g.vs["color"] = [cmap1(betweenness) for betweenness in scaled_vertex_betweenness]
     g.vs["size"]  = ig.rescale(vertex_betweenness, (0.1, 0.5))
     g.es["color"] = [cmap2(betweenness) for betweenness in scaled_edge_betweenness]
     g.es["width"] = ig.rescale(edge_betweenness, (0.5, 1.0))
-
     ig.plot(
-        g, 
-        target=ax, 
+        g,
+        target=ax,
         layout="fruchterman_reingold",
         vertex_frame_width=0.2,
     )
+
+    # Color bars
+    norm1 = ScalarMappable(norm=Normalize(0, max(vertex_betweenness)), cmap=cmap1)
+    norm2 = ScalarMappable(norm=Normalize(0, max(edge_betweenness)), cmap=cmap2)
+    plt.colorbar(norm1, cax=cax1, orientation="horizontal", label='Vertex Betweenness')
+    plt.colorbar(norm2, cax=cax2, orientation="horizontal", label='Edge Betweenness')
+
 
 # Generate Krackhardt Kite Graphs and Watts Strogatz graphs
 random.seed(0)
 g1 = ig.Graph.Famous("Krackhardt_Kite")
 g2 = ig.Graph.Watts_Strogatz(dim=1, size=150, nei=2, p=0.1)
 
-# Plot the graph
-fig, axs = plt.subplots(1, 2, figsize=(10, 8))
-plt.subplots_adjust(bottom=0.3)
-plot_betweenness(g1, fig, axs[0])
-plot_betweenness(g2, fig, axs[1])
-
-# Add "a" and "b" labels for panels
-fig.text(0.05, 0.9, 'a', va='top')
-fig.text(0.55, 0.9, 'b', va='top')
+# Plot the graphs, each with two colorbars for vertex/edge betweenness
+fig, axs = plt.subplots(
+    3, 2,
+    figsize=(7, 6),
+    gridspec_kw=dict(height_ratios=(20, 1, 1)),
+    )
+#plt.subplots_adjust(bottom=0.3)
+plot_betweenness(g1, *axs[:, 0])
+plot_betweenness(g2, *axs[:, 1])
+fig.tight_layout(h_pad=1)
 
 plt.show()
