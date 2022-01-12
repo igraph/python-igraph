2bbb58735b68df66c35f68624dbe147cb1a31a67
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-01-12 17:46:06 +1100

    Remove commented old pydoctor patching lines

--- scripts/patch-pydoctor.sh
+++ scripts/patch-pydoctor.sh
@@ -12,21 +12,10 @@ set -e
 ROOT_FOLDER=$1
 PATCH_FOLDER=$(realpath $2)
 ${ROOT_FOLDER}/.venv/bin/pip uninstall -y pydoctor
-#${ROOT_FOLDER}/.venv/bin/pip install pydoctor==21.2.2
 ${ROOT_FOLDER}/.venv/bin/pip install pydoctor==21.12.0
 PYDOCTOR_DIR=`.venv/bin/python -c 'import os,pydoctor;print(os.path.dirname(pydoctor.__file__))'`
 
 cd "${PYDOCTOR_DIR}"
-## patch is confirmed to work with pydoctor 21.2.2
-#echo "First patch"
-#patch -r deleteme.rej -N -p2 <${PATCH_FOLDER}/pydoctor-21.2.2.patch 2>/dev/null
-#rm -f deleteme.rej
-#
-## patch sphinx extension
-#echo "Second patch"
-#patch -r deleteme.rej -N -p2 <${PATCH_FOLDER}/pydoctor-21.2.2_sphinx_ext.patch 2>/dev/null
-#rm -f deleteme.rej
-
-echo "New patch against 21.12.0"
+echo "Patch against 21.12.0 to fix a bug with C extensions"
 patch -r deleteme.rej -N -p2 <${PATCH_FOLDER}/pydoctor-21.12.0.patch 2>/dev/null
 rm -f deleteme.rej
