c3cfcaa40542fcaa2a08a952c58d8d9f1f7608d6
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-10-10 21:21:57 +1100

    RTD uses python 3.9

--- scripts/rtd_prebuild.sh
+++ scripts/rtd_prebuild.sh
@@ -8,4 +8,4 @@ echo "Modularize pure Python modules"
 /home/docs/checkouts/readthedocs.org/user_builds/igraph/envs/${READTHEDOCS_VERSION}/bin/python3 scripts/patch_modularized_graph_methods.py
 
 echo "NOTE (sigh!): Patch pydoctor to trigger build-finished before RTD extension"
-sed -i 's/on_build_finished)/on_build_finished, priority=490)/' /home/docs/checkouts/readthedocs.org/user_builds/igraph/envs/${READTHEDOCS_VERSION}/lib/python3.10/site-packages/pydoctor/sphinx_ext/build_apidocs.py
+sed -i 's/on_build_finished)/on_build_finished, priority=490)/' /home/docs/checkouts/readthedocs.org/user_builds/igraph/envs/${READTHEDOCS_VERSION}/lib/python3.9/site-packages/pydoctor/sphinx_ext/build_apidocs.py
