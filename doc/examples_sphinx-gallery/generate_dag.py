fe4381e7ff1314b0e1227f2eb0cb32710e19bd1a
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-10-27 18:55:39 +1100

    Tutorial on DAG (#724)

---
+++ doc/examples_sphinx-gallery/generate_dag.py
@@ -0,0 +1,45 @@
+"""
+
+.. _tutorials-dag:
+
+======================
+Directed Acyclic Graph
+======================
+
+This example demonstrates how to create a random directed acyclic graph (DAG), which is useful in a number of contexts including for Git commit history.
+"""
+import igraph as ig
+import matplotlib.pyplot as plt
+import random
+
+
+# %%
+# First, we set a random seed for reproducibility
+random.seed(0)
+
+# %%
+# First, we generate a random undirected graph without loops
+g = ig.Graph.Erdos_Renyi(n=15, p=0.3, directed=False, loops=False)
+
+# %%
+# Then we convert it to a DAG *in place*
+g.to_directed(mode="acyclic")
+
+# %%
+# We can print out a summary of the DAG
+ig.summary(g)
+
+
+# %%
+# Finally, we can plot the graph using the Sugiyama layout from :meth:`igraph.Graph.layout_sugiyama`:
+fig, ax = plt.subplots()
+ig.plot(
+    g,
+    target=ax,
+    layout="sugiyama",
+    vertex_size=15,
+    vertex_color="grey",
+    edge_color="#222",
+    edge_width=1,
+)
+plt.show()
