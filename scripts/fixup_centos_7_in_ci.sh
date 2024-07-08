3f46d384e08a56cc01d6c316eee4b4328761fb58
Tamas Nepusz <ntamas@gmail.com>
2024-07-08 19:52:13 +0200

    fix: update script that fixes the CentOS build

--- scripts/fixup_centos_7_in_ci.sh
+++ scripts/fixup_centos_7_in_ci.sh
@@ -4,3 +4,5 @@
 
 sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
 sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
+
