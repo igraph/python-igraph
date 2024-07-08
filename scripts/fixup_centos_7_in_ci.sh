cc5e7082b1c35c763ffd3d4c753cda6b7c7e1153
Tamas Nepusz <ntamas@gmail.com>
2024-07-08 20:41:53 +0200

    ci: remove CentOS 7 specific hack from CI build

--- scripts/fixup_centos_7_in_ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Workaround for cibuildwheel Docker images using CentOS 7 now that CentOS 7
-# is gone.
-
-sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
-sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
-sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
-
