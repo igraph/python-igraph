90b5a04d6cc8cc574322795eabd68028d81a52ec
Tamas Nepusz <ntamas@gmail.com>
2024-10-24 11:03:27 +0200

    fix(rtd): fix version number of Python in scripts/rtd_prebuild.sh

--- scripts/rtd_prebuild.sh
+++ scripts/rtd_prebuild.sh
@@ -12,4 +12,4 @@ echo "Modularize pure Python modules"
 echo "NOTE: Patch pydoctor to trigger build-finished before RTD extension"
 # see https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx.application.Sphinx.connect
 # see also https://github.com/readthedocs/readthedocs.org/pull/4054 - might or might not be exactly what we are seeing here
-sed -i 's/on_build_finished)/on_build_finished, priority=490)/' /home/docs/checkouts/readthedocs.org/user_builds/igraph/envs/${READTHEDOCS_VERSION}/lib/python3.9/site-packages/pydoctor/sphinx_ext/build_apidocs.py
+sed -i 's/on_build_finished)/on_build_finished, priority=490)/' /home/docs/checkouts/readthedocs.org/user_builds/igraph/envs/${READTHEDOCS_VERSION}/lib/python3.11/site-packages/pydoctor/sphinx_ext/build_apidocs.py
