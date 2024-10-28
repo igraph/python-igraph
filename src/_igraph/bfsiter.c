f1967ec36f119cd2da8e4c147d70b1a9a5d0a852
Tamas Nepusz <ntamas@gmail.com>
2024-10-28 13:14:00 +0100

    chore: drop support for Python 3.8 as it is now EOL

--- src/_igraph/bfsiter.c
+++ src/_igraph/bfsiter.c
@@ -121,10 +121,7 @@ static int igraphmodule_BFSIter_traverse(igraphmodule_BFSIterObject *self,
 				  visitproc visit, void *arg) {
   RC_TRAVERSE("BFSIter", self);
   Py_VISIT(self->gref);
-#if PY_VERSION_HEX >= 0x03090000
-  // This was not needed before Python 3.9 (Python issue 35810 and 40217)
   Py_VISIT(Py_TYPE(self));
-#endif
   return 0;
 }
 
